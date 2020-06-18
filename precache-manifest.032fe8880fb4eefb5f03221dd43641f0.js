52b49df6f333b5099e9d7db1557445344599515d
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2020-06-18 08:48:09 +0530

    Updates

---
+++ precache-manifest.032fe8880fb4eefb5f03221dd43641f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acf1e38a48f27ec306295336dfbe0e3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a96beefd7da33de5766d",
+    "url": "/robofriends/static/css/main.8b7bfaca.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96beefd7da33de5766d",
+    "url": "/robofriends/static/js/main.e908c47e.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
